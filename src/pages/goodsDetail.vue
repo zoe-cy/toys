7c7a2fa594f52bceb71021b9372cdc6f5e3565b0
zoe <oncwnuOlWuL6YqHzIaPdB_DYsZNw@git.weixin.qq.com>
2021-04-21 00:02:37 +0800

    加数据

--- src/pages/goodsDetail.vue
+++ src/pages/goodsDetail.vue
@@ -69,14 +69,25 @@
     <div class="content">
       <div class="box">
         <div v-if="flag == '1'">
-          <el-table :data="tableData" border style="width: 100%">
-            <el-table-column prop="date" label="日期" width="180">
-            </el-table-column>
-            <el-table-column prop="name" label="姓名" width="180">
-            </el-table-column>
-            <el-table-column prop="address" label="地址"> </el-table-column>
-          </el-table>
-          <img src="../assets/teas.png" />
+          <table cellspacing="0" cellpadding="0" class="tables">
+            <tbody>
+              <tr>
+                <td class="tds">产品型号:</td>
+                <td class="t-td">{{goods.id}}</td>
+                <td class="tds">产品尺寸:</td>
+                <td class="t-td">{{goods.size}}</td>
+              </tr>
+              <tr>
+                <td class="tds">产品材质:</td>
+                <td class="t-td">{{goods.texture}}</td>
+                <td class="tds">适合年龄:</td>
+                <td class="t-td">{{goods.age}}</td>
+              </tr>
+            </tbody>
+          </table>
+          <div class="imgs" v-for="item in goods.detail_pics" :key="item">
+            <img :src="item" />
+          </div>
         </div>
         <div v-if="flag == '2'">123</div>
       </div>
@@ -94,16 +105,17 @@ export default {
         title: '积木城堡',
         category: '益智系列',
         category_id: '1',
-        kind: '白色',
         attr: ['进口榉木', '环保水性漆'],
         price: 30,
         old_price: 50,
         sold_num: 22,
+        goods_num: 56,
         size: '20x20x4cm',
         texture: '木制',
         age: '12m+',
-        pics: [require('../assets/goods/10011.png'), require('../assets/goods/10012.png')],
-        pic_url: require('../assets/goods/10011.png')
+        detail_pics: [require('../assets/goods/100121.png')],
+        pics: [require('../assets/goods/100111.png'), require('../assets/goods/100112.png')],
+        pic_url: require('../assets/goods/100111.png')
       },
       serves: ['七天无理由退货', '品质保障']
     }
@@ -145,13 +157,18 @@ export default {
 }
 .left {
   height: 500px;
+  overflow: hidden;
+}
+.left img {
 }
 .el-carousel {
   width: 400px;
   margin: 80px auto 0;
 }
 .el-carousel__item {
+  height: 300px;
   background-size: 100% 100%;
+  border: 1px solid rgb(214, 212, 212);
 }
 .el-button {
   font-weight: normal;
@@ -168,6 +185,8 @@ export default {
 }
 .title {
   font-size: 24px;
+  line-height: 60px;
+  height: 60px;
 }
 .active {
   font-size: 20px;
@@ -216,11 +235,26 @@ export default {
 .tabs li:last-child span {
   border: none;
 }
-.tabs .content {
+.content {
   min-width: 1100px;
   overflow: hidden;
 }
 .content img {
   width: 1100px;
+}
+.tables {
+  border-collapse: collapse;
+  min-width: 1100px;
+}
+.tables td {
+  line-height: 60px;
+}
+.tds {
+  width: 100px;
+  text-align: center;
+}
+.t-td {
+  width: 400px;
+  padding-left: 20px;
 }
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,13 +2,13 @@
   <div class="index">
     <div class="main">
       <div class="box clearfix">
-      <!-- banner z-index2 tabs z-index99-->
+        <!-- banner z-index2 tabs z-index99-->
         <banners></banners>
       </div>
     </div>
-    <div class="goodsbox" v-for="item in goodsList" :key="item.category_id">
+    <div class="goodsbox" v-for="item in goodsTable" :key="item.category_id">
       <div class="header">
-        <span>{{item.category}}</span>
+        <span>{{ item.category }}</span>
         <div class="right" @click="toGoodsList(item.category_id)">
           <span>查看全部</span>
           <span>></span>
@@ -31,22 +31,123 @@ export default {
       tHeight: {
         height: '540px'
       },
-      goodsList: [
+      goodsTable: [
         {
-          id: '1001',
-          title: '积木城堡',
-          category: '益智系列',
           category_id: '1',
-          kind: '白色',
-          attr: ['进口榉木', '环保水性漆'],
-          price: 30,
-          old_price: 50,
-          sold_num: 22,
-          size: '20x20x4cm',
-          texture: '木制',
-          age: '12m+',
-          pics: [require('../assets/goods/10011.png'), require('../assets/goods/10012.png')],
-          pic_url: require('../assets/goods/10011.png')
+          category: '益智系列',
+          goods_list: [
+            {
+              id: '1001',
+              title: '儿童钓鱼玩具磁性鱼宝宝小猫游戏',
+              attr: ['手眼协调', '智力早教'],
+              price: 28,
+              old_price: 32,
+              sold_num: 22,
+              goods_num: 56,
+              type: '钓鱼拼板',
+              age: '24m+',
+              texture: '木制',
+              gender: '中性',
+              pics: [
+                require('../assets/goods/100111.png'),
+                require('../assets/goods/100112.png')
+              ],
+              detail_pics: [
+                require('../assets/goods/100121.png')
+              ],
+              pic_url: require('../assets/goods/100111.png')
+            },
+            {
+              id: '1002',
+              title: '撕不烂认知铁盒玩具',
+              attr: ['益智配对', '早教卡片'],
+              price: 8,
+              old_price: 15,
+              sold_num: 58,
+              goods_num: 96,
+              type: '铁盒认知卡',
+              age: '24m+',
+              texture: '木制',
+              gender: '中性',
+              pics: [
+                require('../assets/goods/100211.png'),
+                require('../assets/goods/100212.png')
+              ],
+              detail_pics: [
+                require('../assets/goods/100221.png')
+              ],
+              pic_url: require('../assets/goods/100211.png')
+            },
+            {
+              id: '1003',
+              title: '婴幼儿拼图',
+              attr: ['宝宝启蒙', '儿童益智'],
+              price: 9,
+              old_price: 32,
+              sold_num: 158,
+              goods_num: 496,
+              type: '卡扣拼图',
+              age: '24m+',
+              texture: '木制',
+              gender: '中性',
+              pics: [
+                require('../assets/goods/100311.png'),
+                require('../assets/goods/100312.png')
+              ],
+              detail_pics: [
+                require('../assets/goods/100321.png')
+              ],
+              pic_url: require('../assets/goods/100311.png')
+            },
+            {
+              id: '1004',
+              title: '磁性苹果圣诞树',
+              attr: ['学数数分苹果', '蒙特教具'],
+              price: 31,
+              old_price: 35,
+              sold_num: 18,
+              goods_num: 76,
+              type: '快乐果园',
+              age: '36m+',
+              texture: '木制',
+              gender: '中性',
+              pics: [
+                require('../assets/goods/100411.png'),
+                require('../assets/goods/100412.png')
+              ],
+              detail_pics: [
+                require('../assets/goods/100421.png')
+              ],
+              pic_url: require('../assets/goods/100411.png')
+            }
+          ]
+        },
+        {
+          category_id: '2',
+          category: '积木系列',
+          goods_list: [
+            {
+              id: '2001',
+              title: '百变立人套柱套塔积木',
+              attr: ['蒙氏早教', '儿童益智'],
+              price: 28,
+              old_price: 32,
+              sold_num: 158,
+              goods_num: 496,
+              type: '百变立人',
+              age: '12m+',
+              texture: '木制',
+              gender: '中性',
+              pics: [
+                require('../assets/goods/200111.png'),
+                require('../assets/goods/200112.png')
+              ],
+              detail_pics: [
+                require('../assets/goods/200121.png')
+              ],
+              pic_url: require('../assets/goods/200111.png')
+            }
+          ]
         }
       ]
     }
